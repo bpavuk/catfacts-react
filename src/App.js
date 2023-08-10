becc586c93b1df94e72d47549833705b0a2e6b7f
bpavuk <boopentiy@gmail.com>
2023-08-10 14:56:57 +0300

    implemented app with kotlin/js library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import catfactsSdk from "catfacts-sdk"
+import React, {useEffect, useState} from 'react';
+
+const sdk = new catfactsSdk.com.bpavuk.catfacts.CatFactsJs()
 
 function App() {
+  const [fact, setFact] = useState(null);
+
+  useEffect(() => {
+    sdk.getCatFact()
+      .then((res) => {
+        console.log(res.fact)
+        setFact(res.fact);
+      })
+      .catch((error) => {
+        console.error('Error fetching cat fact:', error);
+      });
+  }, []); // Run this effect once on component mount
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +34,7 @@ function App() {
         >
           Learn React
         </a>
+        {fact ? <p>Your random cat fact is: {fact}</p> : <p>Loading cat fact...</p>}
       </header>
     </div>
   );
